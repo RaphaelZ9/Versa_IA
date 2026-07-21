ae07c7fb5b2cd1c0430ef4f7471f69eb5dd42478
Raphael Wilson <raphaelwilsonsilva17@gmail.com>
2026-07-21 09:28:03 -0300

    Integrar MemoryProvider ao ContextBuilder

--- app/ai_core/context/providers/__init__.py
+++ app/ai_core/context/providers/__init__.py
@@ -1,9 +1,11 @@
 from .base_context_provider import BaseContextProvider
 from .system_provider import SystemProvider
 from .company_provider import CompanyProvider
+from .memory_provider import MemoryProvider
 
 __all__ = [
     "BaseContextProvider",
     "SystemProvider",
     "CompanyProvider",
+    "MemoryProvider",
 ]
